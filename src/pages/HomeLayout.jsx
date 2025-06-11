368bfb76949a992b4a8321153419e93002e50cbe
Sarthak <timeless.sarthak@gmail.com>
2025-06-11 13:59:37 +0530

    Created Pages and modified App

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,9 @@
+const HomeLayout = ()=>{
+    return (
+        <div>
+         <><h1>this homepage is being opened through the file</h1>
+         <h2>Sorry, through the external file</h2></>
+         </div>
+    )
+}
+export default HomeLayout;
