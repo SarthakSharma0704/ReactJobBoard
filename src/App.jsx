18a693fb29d0f009c278e19dfafa54cf463ca662
Sarthak <timeless.sarthak@gmail.com>
2025-06-16 12:58:49 +0530

    Add existing project to git

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import './App.css'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import './App.css';
+import { HomeLayout, DashboardLayout, Error, Landing } from './pages';
+import { Login,Register } from './components';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <h1>This is my HomePage</h1>
+    element: <HomeLayout />,
+    errorElement: <Error />,
   },
   {
-    path: '/about',
+    path: '/landing',
+    element: <Landing />
+  },
+  {
+    path:'/login',
+    element: <Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  },
+  {
+    path:'/dashboard',
+    element: <DashboardLayout/>
+  },
+  {
+    path:'/about',
     element:(<div>
-      <h1>This is About Page Open now</h1>
-      <h1>What are you thinking today</h1>
-      </div>)
-  }
+                <h1>This is About page open now</h1>
+                <h1>What are you thinking today</h1>
+            </div>)
+  },
 ])
 
 function App() {
   return <RouterProvider router={router}/>
 };
 
-export default App
+export default App;
